a611b8e4845eee355c08cf9ae9b900f94e5b002a
Daniel Lauzon <daniel.lauzon@gmail.com>
2012-12-04 16:37:33 -0500

    remove rebott handler from recipe

--- cookbooks/windows-fromscratch/recipes/forcereboot.rb
+++ cookbooks/windows-fromscratch/recipes/forcereboot.rb
@@ -1,7 +1,9 @@
 
-include_recipe 'windows::reboot_handler'
+# handler include in Vagrant file
+#  include_recipe 'windows::reboot_handler'
 # we want to do this on first boot only...
-Chef::Log.info('Will force reboot: #{any_of_theese}')
+Chef::Log.info('Will force reboot')
+
 windows_reboot 60 do
   reason 'cause chef said so'
   action :request
