938b1c43e2dca77f7ab185fd1ed61064a7df32f0
Daniel Lauzon <daniel.lauzon@gmail.com>
2012-12-07 15:26:00 -0500

    notepad++

--- cookbooks/windows-fromscratch/recipes/packages.rb
+++ cookbooks/windows-fromscratch/recipes/packages.rb
@@ -11,6 +11,12 @@ windows_package "7-Zip 9.20 (x64 edition)" do
   action :install
 end
 
+# install Notepad++ (Y U No Emacs?)
+windows_package "Notepad++" do
+  source "http://download.tuxfamily.org/notepadplus/6.2.2/npp.6.2.2.Installer.exe"
+  action :install
+end
+
 # Google Chrome FTW (MSI installer)
 windows_package "Google Chrome" do
   source "https://dl-ssl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B806F36C0-CB54-4A84-A3F3-0CF8A86575E0%7D%26lang%3Den%26browser%3D3%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dfalse/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi"
